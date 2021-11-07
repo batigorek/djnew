ed074170589197fd3cbffeccb20abbb809a776b0
batigorek <batigorek@gmail.com>
2021-11-08 00:25:38 +0300

    Form

--- women/views.py
+++ women/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -29,7 +31,20 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse('<h1>Add page</h1>')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+
+    else:
+        form = AddPostForm()
+
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def show_post(request, post_slug):
